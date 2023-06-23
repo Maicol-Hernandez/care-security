0a23084f99401803948960a0ca271fb64d4ec1c9
Maicol Hernandez Peralta <maicolhernandez420@gmail.com>
2023-06-23 08:50:02 -0500

    Document and document type paths

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use App\Http\Controllers\ProcessController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\TypeDocumentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,11 +36,20 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    /**
+     * Document
+     */
+    Route::resource('documents', DocumentController::class);
 
     /**
      * Process
      */
     Route::resource('processes', ProcessController::class)->only(['index']);
+
+    /**
+     * Type Document
+     */
+    Route::resource('type-documents', TypeDocumentController::class)->only(['index']);
 });
 
 require __DIR__ . '/auth.php';
