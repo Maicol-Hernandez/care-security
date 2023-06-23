75e577e90a3e56fb209f4598b8a6e5bb7253f7c8
Maicol Hernandez Peralta <maicolhernandez420@gmail.com>
2023-06-23 08:50:50 -0500

    CURD documents

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -2,19 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
+use App\Models\Process;
 use App\Models\Document;
+use App\Models\TypeDocument;
+use App\Services\DocumentService;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Request;
 use App\Http\Requests\StoreDocumentRequest;
 use App\Http\Requests\UpdateDocumentRequest;
 
 class DocumentController extends Controller
 {
+
+    protected DocumentService $documentService;
+
+    public function __construct(DocumentService $documentService)
+    {
+        $this->documentService = $documentService;
+    }
+
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return Inertia::render('Documents/Index', [
+            'documents' => Document::query()
+                ->when(Request::input('search'), function ($query, $search) {
+                    $query->orWhere('code', 'like', "%{$search}%");
+                    $query->orWhere('name', 'like', "%{$search}%");
+                })->with('process', 'typeDocument')->get()
+        ]);
     }
 
     /**
@@ -22,7 +44,13 @@ class DocumentController extends Controller
      */
     public function create()
     {
-        //
+        $processes = Process::all();
+        $typeDocuments = TypeDocument::all();
+
+        return Inertia::render('Documents/Create', [
+            'processes' => $processes,
+            'typeDocuments' => $typeDocuments
+        ]);
     }
 
     /**
@@ -30,7 +58,24 @@ class DocumentController extends Controller
      */
     public function store(StoreDocumentRequest $request)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $document = Document::create($request->all());
+
+            $document->code = $this->documentService->generateCode(
+                $document->process->code,
+                $document->typeDocument->code,
+                $document->id
+            );
+            $document->save();
+            DB::commit();
+        } catch (\Exception $e) {
+            Log::error($e);
+            DB::rollback();
+            return back();
+        }
+
+        return redirect('documents');
     }
 
     /**
@@ -46,7 +91,12 @@ class DocumentController extends Controller
      */
     public function edit(Document $document)
     {
-        //
+        $processes = Process::all();
+        $typeDocuments = TypeDocument::all();
+
+        return Inertia::render('Documents/Edit', [
+            'document' => $document, 'processes' => $processes, 'typeDocuments' => $typeDocuments
+        ]);
     }
 
     /**
@@ -54,7 +104,24 @@ class DocumentController extends Controller
      */
     public function update(UpdateDocumentRequest $request, Document $document)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $document->update($request->all());
+
+            $document->code = $this->documentService->generateCode(
+                $document->process->code,
+                $document->typeDocument->code,
+                $document->id
+            );
+            $document->save();
+            DB::commit();
+        } catch (\Exception $e) {
+            Log::error($e);
+            DB::rollback();
+            return back();
+        }
+
+        return redirect('documents');
     }
 
     /**
@@ -62,6 +129,10 @@ class DocumentController extends Controller
      */
     public function destroy(Document $document)
     {
-        //
+        if ($document->delete()) {
+            return redirect('documents');
+        } else {
+            return back();
+        }
     }
 }
